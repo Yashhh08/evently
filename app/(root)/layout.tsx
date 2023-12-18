1ac655355edb19bdb63a52e9885ac796e3cb638b
Yash Yerunkar <yashyerunkar8@gmail.com>
2023-12-18 20:12:35 +0530

    feat: initial setup

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+import Footer from "@/components/shared/Footer";
+import Header from "@/components/shared/Header";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
+}
